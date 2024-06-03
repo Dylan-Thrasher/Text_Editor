488db9ce27a687c6d06bd3d480093379f25214df
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-06-02 23:07:12 -0400

    updated install.js for client side in src for handlers

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,30 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Added an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    //defers event
+    window.deferredPrompt = event;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    //displays install button
+    butInstall.classList.toggle('hidden', false);
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+});
+
+// Implemented a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const eventPrompt = window.deferredPrompt
+    if (!eventPrompt) {
+        return;
+    }
+    eventPrompt.prompt()
+
+    window.deferredPrompt = null
+    butInstall.classList.toggle('hidden', true)
+});
+
+//  Added an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('J.A.T.E. installed', event);
+    window.deferredPrompt = null;
+});
